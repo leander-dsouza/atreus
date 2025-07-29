bc2decd474ba9797e35a89f6d5528f5e3840e146
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-07-29 23:33:07 +0100

    Improve pre commit (#14)
    * Added codespell to pre-commit hooks.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Added Pyflakes to the ruff configuration.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Added isort to the ruff configuration.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Added Pycodestyle and Pydocstyle checks.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    ---------
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- launch/mapping/slam_toolbox.launch.py
+++ launch/mapping/slam_toolbox.launch.py
@@ -1,72 +1,71 @@
 #! /usr/bin/env python3
-"""
-Launch SLAM Toolbox for mapping with RViz support
-"""
+"""Script to launch Gazebo, RViz, and SLAM Toolbox for the Atreus robot."""
 
 import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    """Launch Function"""
+    """Generate the launch description for Atreus."""
+    pkg_dir = get_package_share_directory("atreus")
 
-    pkg_dir = get_package_share_directory('atreus')
-
-    rviz_config_path = os.path.join(
-        pkg_dir, 'config', 'rviz', 'mapping.rviz')
+    rviz_config_path = os.path.join(pkg_dir, "config", "rviz", "mapping.rviz")
 
     slam_toolbox_params_path = os.path.join(
-        pkg_dir, 'config', 'mapping',
-        'slam_toolbox', 'slam_toolbox_params.yaml'
+        pkg_dir, "config", "mapping", "slam_toolbox", "slam_toolbox_params.yaml"
     )
 
     # Create the launch configuration variables
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    rviz_config = LaunchConfiguration('rviz_config')
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    rviz_config = LaunchConfiguration("rviz_config")
 
     # Declare the launch arguments
     declare_sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time', default_value='True',
-        description='Flag to enable use_sim_time'
+        "use_sim_time", default_value="True", description="Flag to enable use_sim_time"
     )
     declare_rviz_config_arg = DeclareLaunchArgument(
-        'rviz_config', default_value=rviz_config_path,
-        description='Full path to the RViz config file to use'
+        "rviz_config",
+        default_value=rviz_config_path,
+        description="Full path to the RViz config file to use",
     )
 
     # Include the Nodes
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_config],
-        parameters=[{'use_sim_time': use_sim_time}],
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config],
+        parameters=[{"use_sim_time": use_sim_time}],
     )
 
     # Include the Launch files
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
-                [pkg_dir, 'launch', 'description', 'gazebo.launch.py'])),
+            PathJoinSubstitution([pkg_dir, "launch", "description", "gazebo.launch.py"])
+        ),
         launch_arguments={
-            'two_d_lidar_enabled': 'True',
+            "two_d_lidar_enabled": "True",
         }.items(),
     )
 
     slam_toolbox_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution(
-                [FindPackageShare('slam_toolbox'), 'launch', 'online_async_launch.py'])),
+                [FindPackageShare("slam_toolbox"), "launch", "online_async_launch.py"]
+            )
+        ),
         launch_arguments={
-                'use_sim_time': use_sim_time,
-                'slam_params_file': slam_toolbox_params_path,
-        }.items()
+            "use_sim_time": use_sim_time,
+            "slam_params_file": slam_toolbox_params_path,
+        }.items(),
     )
 
     ld = LaunchDescription()
--- launch/navigation/nav2.launch.py
+++ launch/navigation/nav2.launch.py
@@ -1,78 +1,89 @@
+#! /usr/bin/env python3
+"""Script to launch Gazebo, RViz, and Nav2 for the Atreus robot."""
+
 import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 
 def generate_launch_description():
-    pkg_dir = get_package_share_directory('atreus')
+    """Generate the launch description for Atreus."""
+    pkg_dir = get_package_share_directory("atreus")
 
     warehouse_map = os.path.join(
-        pkg_dir, 'config', 'mapping',
-        'maps', 'warehouse_map', 'warehouse_map.yaml')
-
+        pkg_dir, "config", "mapping", "maps", "warehouse_map", "warehouse_map.yaml"
+    )
 
     # Create the launch configuration variables
-    map_yaml_file = LaunchConfiguration('map')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    params_file = LaunchConfiguration('params_file')
-    rviz_config_file = LaunchConfiguration('rviz_config_file')
+    map_yaml_file = LaunchConfiguration("map")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    params_file = LaunchConfiguration("params_file")
+    rviz_config_file = LaunchConfiguration("rviz_config_file")
 
     # Declare the launch arguments
     declare_map_yaml_arg = DeclareLaunchArgument(
-        'map',
+        "map",
         default_value=warehouse_map,
-        description='Full path to map file to load',
+        description="Full path to map file to load",
     )
     declare_use_sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='true',
-        description='Use simulation (Gazebo) clock if true')
+        "use_sim_time",
+        default_value="true",
+        description="Use simulation (Gazebo) clock if true",
+    )
 
     declare_params_file_arg = DeclareLaunchArgument(
-        'params_file',
-        default_value=os.path.join(pkg_dir, 'config', 'navigation', 'nav2_params.yaml'),
-        description='Full path to the ROS2 parameters file to use')
+        "params_file",
+        default_value=os.path.join(pkg_dir, "config", "navigation", "nav2_params.yaml"),
+        description="Full path to the ROS2 parameters file to use",
+    )
 
     declare_rviz_config_file_arg = DeclareLaunchArgument(
-        'rviz_config_file',
-        default_value=os.path.join(pkg_dir, 'config', 'rviz', 'navigation.rviz'),
-        description='Full path to the RViz config file to use')
-
+        "rviz_config_file",
+        default_value=os.path.join(pkg_dir, "config", "rviz", "navigation.rviz"),
+        description="Full path to the RViz config file to use",
+    )
 
     # Include the launch files
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
-                [pkg_dir, 'launch', 'description', 'gazebo.launch.py'])),
+            PathJoinSubstitution([pkg_dir, "launch", "description", "gazebo.launch.py"])
+        ),
         launch_arguments={
-            'two_d_lidar_enabled': 'True',
+            "two_d_lidar_enabled": "True",
         }.items(),
     )
 
     nav2_rviz_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-                        PathJoinSubstitution(
-                [FindPackageShare('nav2_bringup'), 'launch', 'rviz_launch.py'])),
+            PathJoinSubstitution(
+                [FindPackageShare("nav2_bringup"), "launch", "rviz_launch.py"]
+            )
+        ),
         launch_arguments={
-            'use_sim_time': use_sim_time,
-            'rviz_config': rviz_config_file,
+            "use_sim_time": use_sim_time,
+            "rviz_config": rviz_config_file,
         }.items(),
     )
 
     nav2_bringup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution(
-                [FindPackageShare('nav2_bringup'), 'launch', 'bringup_launch.py'])),
+                [FindPackageShare("nav2_bringup"), "launch", "bringup_launch.py"]
+            )
+        ),
         launch_arguments={
-            'map': map_yaml_file,
-            'use_sim_time': use_sim_time,
-            'params_file': params_file,
-        }.items())
-
+            "map": map_yaml_file,
+            "use_sim_time": use_sim_time,
+            "params_file": params_file,
+        }.items(),
+    )
 
     ld = LaunchDescription()
 
