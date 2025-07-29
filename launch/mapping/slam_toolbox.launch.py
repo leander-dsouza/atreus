59fb190daf1c15345675e1a96ab4ae56a03f33ce
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-07-29 21:19:25 +0100

    Added nav2 support (#12)
    * Removed outdated navigation configuration files.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Disabled shadows to facilitate faster simulation times.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Migrated to skid-steer drive.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Adjusted gazebo launch to include no sensors upon launch.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Updated nav2 launch to use the latest jazzy version.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    * Updated documentation to update mapping and navigation instructions.
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
    
    ---------
    
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- launch/mapping/slam_toolbox.launch.py
+++ launch/mapping/slam_toolbox.launch.py
@@ -9,6 +9,7 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 
 
@@ -16,52 +17,66 @@ def generate_launch_description():
     """Launch Function"""
 
     pkg_dir = get_package_share_directory('atreus')
-    slam_toolbox_pkg = get_package_share_directory('slam_toolbox')
 
-    rviz_config_arg = DeclareLaunchArgument(
-        'rviz_config', default_value='mapping.rviz',
-        description='RViz config file'
-    )
-
-    sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time', default_value='True',
-        description='Flag to enable use_sim_time'
-    )
-
-    slam_toolbox_launch_path = os.path.join(
-        slam_toolbox_pkg,
-        'launch',
-        'online_async_launch.py'
-    )
+    rviz_config_path = os.path.join(
+        pkg_dir, 'config', 'rviz', 'mapping.rviz')
 
     slam_toolbox_params_path = os.path.join(
         pkg_dir, 'config', 'mapping',
         'slam_toolbox', 'slam_toolbox_params.yaml'
     )
 
+    # Create the launch configuration variables
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    rviz_config = LaunchConfiguration('rviz_config')
+
+    # Declare the launch arguments
+    declare_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', default_value='True',
+        description='Flag to enable use_sim_time'
+    )
+    declare_rviz_config_arg = DeclareLaunchArgument(
+        'rviz_config', default_value=rviz_config_path,
+        description='Full path to the RViz config file to use'
+    )
+
+    # Include the Nodes
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
-        arguments=['-d', PathJoinSubstitution(
-            [pkg_dir, 'config', 'rviz', LaunchConfiguration('rviz_config')])],
-        parameters=[
-            {'use_sim_time': LaunchConfiguration('use_sim_time')},
-        ]
+        name='rviz2',
+        arguments=['-d', rviz_config],
+        parameters=[{'use_sim_time': use_sim_time}],
+    )
+
+    # Include the Launch files
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [pkg_dir, 'launch', 'description', 'gazebo.launch.py'])),
+        launch_arguments={
+            'two_d_lidar_enabled': 'True',
+        }.items(),
     )
 
     slam_toolbox_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(slam_toolbox_launch_path),
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare('slam_toolbox'), 'launch', 'online_async_launch.py'])),
         launch_arguments={
-                'use_sim_time': LaunchConfiguration('use_sim_time'),
+                'use_sim_time': use_sim_time,
                 'slam_params_file': slam_toolbox_params_path,
         }.items()
     )
 
     ld = LaunchDescription()
 
-    ld.add_action(rviz_config_arg)
-    ld.add_action(sim_time_arg)
+    ld.add_action(declare_sim_time_arg)
+    ld.add_action(declare_rviz_config_arg)
+
     ld.add_action(rviz_node)
+
+    ld.add_action(gazebo_launch)
     ld.add_action(slam_toolbox_launch)
 
     return ld
