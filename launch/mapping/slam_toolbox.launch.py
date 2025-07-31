d78ef9d317fe63a7c60d384a1f87e602343cdbbd
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-07-31 01:01:23 +0000

    Added ament linters to the linting workflow.
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- launch/mapping/slam_toolbox.launch.py
+++ launch/mapping/slam_toolbox.launch.py
@@ -1,70 +1,92 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
+
+# Copyright (c) 2025, Leander Stephen Desouza
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 """Script to launch Gazebo, RViz, and SLAM Toolbox for the Atreus robot."""
 
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
-def generate_launch_description():
+
+def generate_launch_description() -> LaunchDescription:
     """Generate the launch description for Atreus."""
-    pkg_dir = get_package_share_directory("atreus")
+    pkg_dir = get_package_share_directory('atreus')
 
-    rviz_config_path = os.path.join(pkg_dir, "config", "rviz", "mapping.rviz")
+    rviz_config_path = os.path.join(pkg_dir, 'config', 'rviz', 'mapping.rviz')
 
     slam_toolbox_params_path = os.path.join(
-        pkg_dir, "config", "mapping", "slam_toolbox", "slam_toolbox_params.yaml"
+        pkg_dir, 'config', 'mapping', 'slam_toolbox', 'slam_toolbox_params.yaml'
     )
 
     # Create the launch configuration variables
-    use_sim_time = LaunchConfiguration("use_sim_time")
-    rviz_config = LaunchConfiguration("rviz_config")
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    rviz_config = LaunchConfiguration('rviz_config')
 
     # Declare the launch arguments
     declare_sim_time_arg = DeclareLaunchArgument(
-        "use_sim_time", default_value="True", description="Flag to enable use_sim_time"
+        'use_sim_time', default_value='True', description='Flag to enable use_sim_time'
     )
     declare_rviz_config_arg = DeclareLaunchArgument(
-        "rviz_config",
+        'rviz_config',
         default_value=rviz_config_path,
-        description="Full path to the RViz config file to use",
+        description='Full path to the RViz config file to use',
     )
 
     # Include the Nodes
     rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        arguments=["-d", rviz_config],
-        parameters=[{"use_sim_time": use_sim_time}],
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config],
+        parameters=[{'use_sim_time': use_sim_time}],
     )
 
     # Include the Launch files
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([pkg_dir, "launch", "description", "gazebo.launch.py"])
+            PathJoinSubstitution([pkg_dir, 'launch', 'description', 'gazebo.launch.py'])
         ),
         launch_arguments={
-            "two_d_lidar_enabled": "True",
+            'two_d_lidar_enabled': 'True',
         }.items(),
     )
 
     slam_toolbox_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution(
-                [FindPackageShare("slam_toolbox"), "launch", "online_async_launch.py"]
+                [FindPackageShare('slam_toolbox'), 'launch', 'online_async_launch.py']
             )
         ),
         launch_arguments={
-            "use_sim_time": use_sim_time,
-            "slam_params_file": slam_toolbox_params_path,
+            'use_sim_time': use_sim_time,
+            'slam_params_file': slam_toolbox_params_path,
         }.items(),
     )
 
